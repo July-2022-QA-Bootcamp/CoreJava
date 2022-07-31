4b29fa830a38405b5ca6072cde7748ead2185e7f
TofaelKabir <tofaelkabir@gmail.com>
2022-07-31 19:47:09 -0400

    update

--- src/lec09_02_java_constructor_final/EmployeeTest.java
+++ src/lec09_02_java_constructor_final/EmployeeTest.java
@@ -5,12 +5,12 @@ public class EmployeeTest {
 		 // default Constructor initialized
 		Employee employee = new Employee();
 				
-		//Parameterized Constructor initialized	
-		Employee employee1 = new Employee("Arman S", 0137653, 'M', true); 
-		Employee employee2 = new Employee("Sanjeeda T", 2237653, 'F', false); 
-		Employee employee3 = new Employee("Abdullah Rafi", 1137653, 'M', false); 
-		Employee employee4 = new Employee("Sharon G", 9137653, 'F', true); 
-		Employee employee5 = new Employee("Mizanur R", 5137653, 'M', true); 
+		// Parameterized Constructor initialized	
+		Employee arman = new Employee("Arman S", 0137653, 'M', true); 
+		Employee sanjeeda = new Employee("Sanjeeda T", 2237653, 'F', false); 
+		Employee rafi = new Employee("Abdullah Rafi", 1137653, 'M', false); 
+		Employee sharon = new Employee("Sharon G", 9137653, 'F', true); 
+		Employee mizan = new Employee("Mizanur R", 5137653, 'M', true); 
 				
 	}
 
